d56288065e6e25df7d84d6f5698d5f1770fda4ef
Mellorison <mellorison@gmail.com>
2020-10-21 05:41:04 +0000

    Added main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+HOMEWORK #1 : VARIABLES
+
+ATTRIBUTES OF MY FAVORITE SONG
+"""
+
+#Name of Artist
+Artist = "Soundgarden"
+
+#Music Genre
+Genre = "Grunge"
+
+#Song Duration
+DurationInSeconds = 328
+
+#Year of song release
+YearReleased = "May 13, 1994"
+
+#Year the song was recorded
+YearRecorded = "July - September, 1993"
+
+
+print(Artist)
+print(Genre)
+print(DurationInSeconds)
+print(YearReleased)
+print(YearRecorded)

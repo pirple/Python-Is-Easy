b7dcf7a725513cbfc5b2e18d2980d13a53841570
Mellorison <mellorison@gmail.com>
2020-10-27 05:32:05 +0000

    Assignment 2

---
+++ mainone.py
@@ -0,0 +1,10 @@
+#def Artist ():
+#	Artist = "Chris Cornell"
+#	Artist = "Soundgarden"
+#	print(Artist)
+
+	 
+def MusicAttribs():
+     Artist = "Soundgarden"
+     Artist = "Chris Cornell"
+     print(Artist)
